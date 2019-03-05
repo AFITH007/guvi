e27535ddeb66c9372f12fa437eb8717c3b080fcf
AFITH007 <31383555+AFITH007@users.noreply.github.com>
2019-03-05 11:16:29 +0530

    Create stop.java

---
+++ stop.java
@@ -0,0 +1,14 @@
+
+import java.lang.*; 
+import java.io.*; 
+import java.util.*; 
+class ReverseNumber
+{
+   public static void main(String args[])
+   {
+      
+      Scanner in = new Scanner(System.in);
+      String S = in.nextLine();
+      System.out.println(S+".");
+   }
+}
